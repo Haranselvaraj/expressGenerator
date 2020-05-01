336e82fd2488f96fc6a945e43874ae662767b125
Haranselvaraj <hariharans970@gmail.com>
2020-05-01 09:40:55 +0530

    mongo rest api

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ const promoRouter = require('./routes/promoRouter');
 
 const leaderRouter = require('./routes/leaderRouder');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+  console.log('Connected correctly to server');
+},(err) => { console.log(err);}); 
+
+
+
 var app = express();
 
 // view engine setup
